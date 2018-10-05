3c5b9d0caf0b38c50c8d156392cff2617008b3d6
unknown <vr@marconicivitavecchia.it>
2018-10-05 15:40:58 +0200

    Use our server and JSON support

--- Assets/MQTT/scripts/test/mqttTest.cs
+++ Assets/MQTT/scripts/test/mqttTest.cs
@@ -8,33 +8,42 @@ using uPLibrary.Networking.M2Mqtt.Exceptions;
 
 using System;
 
+[Serializable]
+class Sonoff { // this is just for test, waiting the real struct from Melita
+	public String isOn = "false";
+}
+
 public class mqttTest : MonoBehaviour {
 	private MqttClient client;
 	// Use this for initialization
 	void Start () {
 		// create client instance 
-		client = new MqttClient(IPAddress.Parse("143.185.118.233"),8080 , false , null ); 
-		
+		// iot.eclipse.org:1883 sonoff17/out sonoff17/in
+		Debug.Log("Start...");
+		client = new MqttClient("iot.eclipse.org",1883 , false , null ); 
+
 		// register to message received 
 		client.MqttMsgPublishReceived += client_MqttMsgPublishReceived; 
 		
 		string clientId = Guid.NewGuid().ToString(); 
 		client.Connect(clientId); 
 		
-		// subscribe to the topic "/home/temperature" with QoS 2 
-		client.Subscribe(new string[] { "hello/world" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
+		client.Subscribe(new string[] { "sonoff17/out" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
 
 	}
 	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
 	{ 
-
-		Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message)  );
+		Sonoff sonoff = JsonUtility.FromJson<Sonoff>(System.Text.Encoding.UTF8.GetString(e.Message));
+		Debug.Log("Lamp is: " + sonoff.isOn );
 	} 
 
 	void OnGUI(){
 		if ( GUI.Button (new Rect (20,40,80,20), "Level 1")) {
 			Debug.Log("sending...");
-			client.Publish("hello/world", System.Text.Encoding.UTF8.GetBytes("Sending from Unity3D!!!"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			Sonoff sonoff = new Sonoff();
+			sonoff.isOn = "true";
+			string jsonMsg = JsonUtility.ToJson(sonoff);
+			client.Publish("sonoff17/out", System.Text.Encoding.UTF8.GetBytes(jsonMsg), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
 			Debug.Log("sent");
 		}
 	}
