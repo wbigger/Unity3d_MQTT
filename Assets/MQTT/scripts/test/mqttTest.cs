ed039a93ec0f3860acede0362c434fef416efa56
unknown <vr@marconicivitavecchia.it>
2018-10-08 15:47:12 +0200

    Separate in and out channels

--- Assets/MQTT/scripts/test/mqttTest.cs
+++ Assets/MQTT/scripts/test/mqttTest.cs
@@ -9,12 +9,30 @@ using uPLibrary.Networking.M2Mqtt.Exceptions;
 using System;
 
 [Serializable]
-class Sonoff { // this is just for test, waiting the real struct from Melita
-	public String isOn = "false";
+// In: command
+class SonoffCmd {
+    public string id = "000";
+	public string type = "generic";
+	public string cmd = "NONE"; 
 }
+
+// Out: feedback
+[Serializable]
+class SonoffFbk {
+	public string id = "000";
+	public string type = "generic";
+	public string state = "unknown"; 
+}
+
 
 public class mqttTest : MonoBehaviour {
 	private MqttClient client;
+
+	private bool refreshState = false;
+	private bool lampEnableFeedbackState = false;
+
+	public Light lamp;
+	
 	// Use this for initialization
 	void Start () {
 		// create client instance 
@@ -28,29 +46,44 @@ public class mqttTest : MonoBehaviour {
 		string clientId = Guid.NewGuid().ToString(); 
 		client.Connect(clientId); 
 		
+		// listen to the state "out" topic
 		client.Subscribe(new string[] { "sonoff17/out" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
 
 	}
 	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
 	{ 
-		Sonoff sonoff = JsonUtility.FromJson<Sonoff>(System.Text.Encoding.UTF8.GetString(e.Message));
-		Debug.Log("Lamp is: " + sonoff.isOn );
+		SonoffFbk lampOut = JsonUtility.FromJson<SonoffFbk>(System.Text.Encoding.UTF8.GetString(e.Message));
+		Debug.Log("Feedback lamp state: " + lampOut.state );
+		lampEnableFeedbackState = string.Compare(lampOut.state,"ON") == 0;
+		refreshState = true;
 	} 
 
 	void OnGUI(){
 		if ( GUI.Button (new Rect (20,40,80,20), "Level 1")) {
-			Debug.Log("sending...");
-			Sonoff sonoff = new Sonoff();
-			sonoff.isOn = "true";
-			string jsonMsg = JsonUtility.ToJson(sonoff);
+			// SonoffIn lampIn = new SonoffIn();
+			// lampIn.cmd = "ON";
+			// Debug.Log("sending cmd " + lampIn.cmd + "...");
+			// string jsonMsg = JsonUtility.ToJson(lampIn);
+			// write the new state on the "in" topic
+			//client.Publish("sonoff17/in", System.Text.Encoding.UTF8.GetBytes(jsonMsg), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			
+			// DEBUG FIXME just for debug, send to in topic to close the loop
+			SonoffFbk lampOut = new SonoffFbk();
+			lampOut.state = "ON";
+			Debug.Log("sending cmd " + lampOut.state + "...");
+			string jsonMsg = JsonUtility.ToJson(lampOut);
 			client.Publish("sonoff17/out", System.Text.Encoding.UTF8.GetBytes(jsonMsg), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			
 			Debug.Log("sent");
 		}
+		
 	}
 	// Update is called once per frame
 	void Update () {
-
-
-
+		if (refreshState) {
+			lamp.enabled = lampEnableFeedbackState;
+			Debug.Log("update lamp state: "+lamp.enabled);
+			refreshState = false;
+		}
 	}
 }
---
+++ Assets/spotlight_test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spotlight_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Ciao");
+		GetComponent<Light>().intensity = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
